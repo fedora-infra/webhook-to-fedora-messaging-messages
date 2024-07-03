5497510b369565de3ff66a8b93fc5208411ea5dd
Super User <barankara5@gmail.com>
2024-07-04 01:25:07 +0300

    fix for formatting

--- webhook_to_fedora_messaging_messages/github/github.py
+++ webhook_to_fedora_messaging_messages/github/github.py
@@ -10,26 +10,26 @@ class GithubMessageV1(Webhook2FedMsgBase):
     @property
     def app_name(self):
         return "Github"
-    
+
     @property
     def target_id(self):
         return self.body['headers']['X-Github-Hook-Installation-Target-Id']
-    
+
     @property
     def signature(self):
         """SHA1 signature of the request"""
         return self.body['headers']['X-Hub-Signature']
-    
+
     @property
     def delivery(self):
         """A globally unique identifier (GUID) to identify the event"""
         return self.body['headers']['X-Github-Delivery']
-    
+
     @property
     def signature_sha256(self):
         """SHA1-256 signature of the request"""
         return self.body['headers']['X-Hub-Signature-256']
-    
+
     @property
     def event_name(self):
         return self.body['headers']["X-Github-Event"]
@@ -41,12 +41,14 @@ class GithubMessageV1(Webhook2FedMsgBase):
     @property
     def summary(self):
         repo_name = self.body['body']['repository']['full_name']
-        return self.agent_name + " created " + self.event_name + (" on " + repo_name if repo_name is not None else "")
+        return self.agent_name + " created " + self.event_name + \
+            (" on " + repo_name if repo_name is not None else "")
+
 
     def __str__(self):
         if self.event_type == "repository":
             return summarize_repository_event(self.event_name, self.body['body'])
-
+    
     body_schema = {
         "id": "http://fedoraproject.org/message-schema/webhook-to-fedora-message",
         "$schema": "http://json-schema.org/draft-04/schema#",
@@ -68,7 +70,7 @@ class GithubMessageV1(Webhook2FedMsgBase):
                     "X-Hub-Signature",
                     "X-Hub-Signature-256",
                     "X-Github-Delivery"
-                    
+
                 ],
                 "properties": {
                     "X-Github-Event": {"type": "string"},
