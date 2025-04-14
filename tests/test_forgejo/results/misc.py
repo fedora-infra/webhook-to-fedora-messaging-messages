36537306947826f362096d3a25e73bc348e6879b
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2025-04-14 09:36:51 +0000

    Include functionality tests for newly added schema
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

---
+++ tests/test_forgejo/results/misc.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2024-2025 Contributors to the Fedora Project
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+"""
+Result specification for Forgejo "misc" event
+"""
+
+summary = "testuser-w2fm created misc on fedora/w2fm-test"
+
+specification = """Event type not supported"""
+
+hashes = {
+    "sha160": "sha1=4d2c9d24e8b3d1d2d55ca93212d07d0dadb33e0e",
+    "sha256": "sha256=7841feec1aa7a37fa831fb2e01313633660db655ac5923c7dc540e52c01d4e66",
+}
