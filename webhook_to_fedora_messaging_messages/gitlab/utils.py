e8b10bc41f46fc6c81fb64ea2b4bb6228d3650ae
Shounak Dey <shounakdey@ymail.com>
2025-05-18 20:08:30 +0530

    Add W2FM schema for GitLab support
    Signed-off-by: Shounak Dey <shounakdey@ymail.com>

---
+++ webhook_to_fedora_messaging_messages/gitlab/utils.py
@@ -0,0 +1,112 @@
+# SPDX-FileCopyrightText: 2024 Contributors to the Fedora Project
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+
+def summarize_repository_event(event_type: str, data: dict) -> str:
+    """
+    Obtain text specification based on event type
+    """
+    if event_type == "Push Hook":
+        return _summarize_push_event(data)
+    elif event_type == "Merge Request Hook":
+        return _summarize_pull_request_event(data)
+    elif event_type == "Issue Hook":
+        return _summarize_issues_event(data)
+    elif event_type == "Note Hook":
+        return _summarize_issue_comment_event(data)
+    else:
+        return "Event type not supported"
+
+
+def _summarize_push_event(data: dict) -> str:
+    """
+    Obtain text specification for push event
+    """
+    repository_name = data["repository"]["name"]
+    repository_url = data["repository"]["homepage"]
+    pusher = data["user_username"]
+    pusher_url = f"https://gitlab.com/{pusher}"
+    branch = data["ref"].split("/")[-1]
+    commits = "\n".join(
+        [
+            f"- {commit['id'][0:7]}: {commit['message'].splitlines()[0]} by {commit['author']['name']} ({commit['url']})"  # noqa: E501
+            for commit in data["commits"]
+        ]
+    )
+    return (
+        f"Event: Push\n"
+        f"Repository: {repository_name} ({repository_url})\n"
+        f"Pusher: {pusher} ({pusher_url})\n"
+        f"Branch: {branch}\n"
+        f"Commits:\n{commits}"
+    )
+
+
+def _summarize_pull_request_event(data: dict) -> str:
+    """
+    Obtain text specification for pull request event
+    """
+    repository_name = data["repository"]["name"]
+    repository_url = data["repository"]["homepage"]
+    pr_title = data["object_attributes"]["title"]
+    pr_url = data["object_attributes"]["url"]
+    pr_author = data["user"]["username"]
+    pr_source_branch = data["object_attributes"]["source_branch"]
+    pr_dest_branch = data["object_attributes"]["target_branch"]
+    pr_reviewers = ", ".join([reviewer["username"] for reviewer in data["reviewers"]])
+    status = data["object_attributes"]["state"]
+    return (
+        f"Event: Pull Request\n"
+        f"Repository: {repository_name} ({repository_url})\n"
+        f"Title: {pr_title}\n"
+        f"URL: {pr_url}\n"
+        f"Author: {pr_author}\n"
+        f"Status: {status}\n"
+        f"Source Branch: {pr_source_branch}\n"
+        f"Destination Branch: {pr_dest_branch}\n"
+        f"Requested Reviewers: {pr_reviewers}"
+    )
+
+
+def _summarize_issues_event(data: dict) -> str:
+    """
+    Obtain text specification for issues event
+    """
+    repository_name = data["repository"]["name"]
+    repository_url = data["repository"]["homepage"]
+    issue_title = data["object_attributes"]["title"]
+    issue_url = data["object_attributes"]["url"]
+    issue_author = data["user"]["username"]
+    issue_labels = ", ".join([label["title"] for label in data["object_attributes"]["labels"]])
+    status = data["object_attributes"]["state"]
+    return (
+        f"Event: Issue\n"
+        f"Repository: {repository_name} ({repository_url})\n"
+        f"Title: {issue_title}\n"
+        f"URL: {issue_url}\n"
+        f"Author: {issue_author}\n"
+        f"Status: {status}\n"
+        f"Labels: {issue_labels}"
+    )
+
+
+def _summarize_issue_comment_event(data: dict) -> str:
+    """
+    Obtain text specification for issue comment event
+    """
+    repository_name = data["repository"]["name"]
+    repository_url = data["repository"]["homepage"]
+    issue_title = data["issue"]["title"]
+    issue_url = data["issue"]["url"]
+    comment_author = data["user"]["username"]
+    comment_body = data["object_attributes"]["description"]
+    comment_url = data["object_attributes"]["url"]
+    return (
+        f"Event: Issue Comment\n"
+        f"Repository: {repository_name} ({repository_url})\n"
+        f"Issue: {issue_title} ({issue_url})\n"
+        f"Comment Author: {comment_author}\n"
+        f"Comment: {comment_body}\n"
+        f"Comment URL: {comment_url}"
+    )
---
+++ webhook_to_fedora_messaging_messages/gitlab/v1.py
@@ -0,0 +1,71 @@
+# SPDX-FileCopyrightText: 2024 Contributors to the Fedora Project
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+import typing
+
+from ..base import Webhook2FedMsgBase
+from .utils import summarize_repository_event
+
+
+class GitLabMessageV1(Webhook2FedMsgBase):
+    @property
+    def app_name(self) -> str:
+        """Application name on Fedora Messaging"""
+        return "GitLab"
+
+    @property
+    def target_id(self) -> str:
+        """Unique ID for each webhook"""
+        return self.body["headers"]["x-gitlab-webhook-uuid"]
+
+    @property
+    def delivery(self) -> str:
+        """Unique ID for webhooks events"""
+        return self.body["headers"]["x-gitlab-event-uuid"]
+
+    @property
+    def event_name(self) -> str:
+        """Name of the GitLab event"""
+        return self.body["headers"]["x-gitlab-event"]
+
+    @property
+    def summary(self) -> str:
+        """Summary of the GitLab event"""
+        repo_name = self.body["body"]["repository"]["name"]
+        text = f"{self.agent_name} created {self.event_name}"
+        if repo_name is not None:
+            text = f"{text} on {repo_name}"
+        return text
+
+    def __str__(self) -> str:
+        """Specification of the GitLab event"""
+        return summarize_repository_event(self.event_name, self.body["body"])
+
+    body_schema: typing.ClassVar = {
+        "id": "http://fedoraproject.org/message-schema/webhook-to-fedora-message",
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "description": "Messages from gitlab via webhook",
+        "type": "object",
+        "required": ["body", "headers"],
+        "properties": {
+            "body": {
+                "description": "The body of the webhook POST request from GitLab",
+                "type": "object",
+            },
+            "headers": {
+                "description": "The headers of the webhook POST request from GitLab",
+                "type": "object",
+                "required": [
+                    "x-gitlab-event",
+                    "x-gitlab-webhook-uuid",
+                    "x-gitlab-event-uuid",
+                ],
+                "properties": {
+                    "x-gitlab-event": {"type": "string"},
+                    "x-gitlab-webhook-uuid": {"type": "string"},
+                    "x-gitlab-event-uuid": {"type": "string"},
+                },
+            },
+        },
+    }
